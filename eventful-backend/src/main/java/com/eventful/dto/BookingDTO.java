7a6e125ccecf49758da64f0121bf0ff9db0a9ad3
Adedayo-Data <theophilusdayo@outlook.com>
2025-08-13 22:19:49 +0100

    FEAT: Booking DTO created to transfer data

---
+++ eventful-backend/src/main/java/com/eventful/dto/BookingDTO.java
@@ -0,0 +1,16 @@
+package com.eventful.dto;
+
+public class BookingDTO {
+
+    private String venue;
+    private String user;
+    private String userEmail;
+    private String date;
+    private String time;
+    private String status;
+    private Double amount;
+    private int guests;
+    private String bookingId;
+
+
+}
