2db9445aa805336653f93c7f171b46ccebee2f76
Adedayo-Data <theophilusdayo@outlook.com>
2025-08-20 14:00:43 +0100

    FIX: Jwt Token expiration time increased

--- eventful-backend/src/main/java/com/eventful/security/JwtService.java
+++ eventful-backend/src/main/java/com/eventful/security/JwtService.java
@@ -26,7 +26,7 @@ public class JwtService {
         return Jwts.builder()
                 .subject(users.getEmail())
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 10000 * 60 * 60))
+                .expiration(new Date(System.currentTimeMillis() + 100000 * 60 * 60))
                 .claims(claim)
                 .signWith(Keys.hmacShaKeyFor(constant.JWTKEY.getBytes()))
                 .compact();
